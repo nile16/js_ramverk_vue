b6c8d15658bf1523df19efdc6fc29c6a6eadf5f4
nile16 <nils.leandersson@gmail.com>
2019-12-17 00:01:08 +0100

    kmom06

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -26,21 +26,22 @@
 import Nav from './Nav.vue';
 
 export default {
-  name: 'Chat',
-  components: {
-    Nav,
-  },
-  data() {
-    return {
-        nickName: "",
-        ws: false
-    }
-  },
+    name: 'Chat',
+    components: {
+      Nav,
+    },
+    data() {
+        return {
+            nickName: "",
+            ws: false
+        }
+    },
     mounted() {
         document.getElementById('nick-button').addEventListener('click', this.saveNick);
         document.getElementById('send-button').addEventListener('click', this.sendMessage);
 
         this.ws = new WebSocket('wss://me-api.nile16.me');
+        //this.ws = new WebSocket('ws://localhost:1337');
 
         let that = this;
 
@@ -53,25 +54,40 @@ export default {
             that.ws.onclose = function() {
             };
         }
-  },
-  methods: {
-      saveNick: function() {
-          this.nickName = document.getElementById('nick-field').value;
-          document.getElementById('nickDiv').style.display = "none";
-          document.getElementById('chatDiv').style.display = "block";
-          this.ws.send(this.nickName + " joined.");
-      },
-      sendMessage: function() {
-          let msg = this.nickName + ": " + document.getElementById('message-field').value;
-          this.ws.send(msg);
-          document.getElementById('message-field').value = "";
-      },
-      printMessage: function(msg) {
-          let chat = document.getElementById('chat-field');
-          chat.innerText += msg + "\n";
-          chat.scrollTo(0, chat.scrollHeight);
-      }
-  }
+    },
+    methods: {
+        saveNick: function() {
+            this.nickName = document.getElementById('nick-field').value;
+            document.getElementById('nickDiv').style.display = "none";
+            document.getElementById('chatDiv').style.display = "block";
+            this.ws.send(JSON.stringify({
+                nick: this.nickName,
+                join: true
+            }));
+        },
+        sendMessage: function() {
+            this.ws.send(JSON.stringify({
+                nick: this.nickName,
+                text: document.getElementById('message-field').value
+            }));
+            document.getElementById('message-field').value = "";
+        },
+        printMessage: function(msgJson) {
+            let chat = document.getElementById('chat-field');
+            let msg = JSON.parse(msgJson);
+
+            for (let i = 0; i < msg.length; i++) {
+                let time = ( new Date(msg[i].time)).toLocaleTimeString();
+
+                if (msg[i].join)
+                    chat.innerHTML += "(" + time + ") '" + msg[i].nick + "' joined!<br>";
+                else
+                    chat.innerHTML += "(" + time + ") " + msg[i].nick + ": " + msg[i].text + "<br>";
+            }
+
+            chat.scrollTo(0, chat.scrollHeight);
+        }
+    }
 }
 
 
